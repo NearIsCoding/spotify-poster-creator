2fc17acc65978c9adb354f2d3c4d5a6d52b2ca45
NearIsCoding <conansebastian@gmail.com>
2024-06-18 21:13:09 -0500

    Add basic form and post to an url

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from spotify_api_controller import SpotifyToken
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,9 +7,22 @@ spotifyToken = SpotifyToken()
 token = spotifyToken.get_token()
 print(token)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
     return render_template('home.html')
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    if request.method == 'POST':
+        artistName = request.form['artistName']
+        return redirect(url_for('show_info', artistName=artistName))
+    return render_template('search.html')
+
+
+@app.route('/show_info')
+def show_info():
+    artistName = request.args.get('artistName')
+    return render_template('show_info.html', artistName=artistName)
 
 if __name__ == '__main__':
     app.run(debug=True)
